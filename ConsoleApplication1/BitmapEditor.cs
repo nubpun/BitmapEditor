6734a891d3e8d653bec4a4dfc4c56b06251b7606
Mayorov Maxim <nub.pun@yandex.ru>
2016-11-11 17:07:18 +0500

    complete

---
+++ ConsoleApplication1/BitmapEditor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    class BitmapEditor:IDisposable
+    {
+        private Bitmap bmp;
+        private BitmapData bmpData;
+        private byte[] rgbValues;
+        public BitmapEditor(Bitmap bmp)
+        {
+            this.bmp = bmp;
+            bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            int numBytes = (bmp.Width * 3 + 1) * bmp.Height;
+            rgbValues = new byte[numBytes];
+            Marshal.Copy(bmpData.Scan0, rgbValues, 0, numBytes);
+        }
+        public void SetPixel(int x, int y, byte r, byte g, byte b)
+        {
+            int pos = x * (bmp.Width * 3 + 1) + y * 3;
+            rgbValues[pos] = b;
+            rgbValues[pos + 1] = g;
+            rgbValues[pos + 2] = r;
+        }
+
+        public void Dispose()
+        {
+            Marshal.Copy(rgbValues, 0, bmpData.Scan0, rgbValues.Length);
+            bmp.UnlockBits(bmpData);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitmap = (Bitmap)Bitmap.FromFile("1.bmp");
+            using (var bitmapEditor = new BitmapEditor(bitmap))
+            {
+                for (int i = 0; i < bitmap.Height; i++)
+                    for (int j = 0; j < bitmap.Width; j++)
+                    {
+                        if (j % 3 == 0)
+                            bitmapEditor.SetPixel(i, j, 255, 0, 0);
+                        if (j % 3 == 1)
+                            bitmapEditor.SetPixel(i, j, 0, 255, 0);
+                        if (j % 3 == 2)
+                            bitmapEditor.SetPixel(i, j, 0, 0, 255);
+                    }
+            }
+            bitmap.Save("new.bmp");
+        }
+    }
+}
